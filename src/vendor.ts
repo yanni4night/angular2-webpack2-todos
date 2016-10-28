7e3d0f8d556f88e36cafa18cfed97fa1e8280d5d
yinyong02 <yinyong02@baidu.com>
2016-10-28 17:46:04 +0800

    import modules

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,22 +1,11 @@
-/**
-  * Copyright (C) 2016 yanni4night.com
-  * vendor.ts
-  *
-  * changelog
-  * 2016-10-27[17:26:47]:revised
-  *
-  * @author yanni4night@gmail.com
-  * @version 0.1.0
-  * @since 0.1.0
-  */
 // Angular
 import '@angular/platform-browser';
-//import '@angular/platform-browser-dynamic';
+import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
 import '@angular/forms';
-//import '@angular/http';
-//import '@angular/router';
+import '@angular/http';
+import '@angular/router';
 // RxJS
 import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
